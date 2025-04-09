5c915314fd28c309bfadfb16f9285d1517df748e
Andre Gleichner <andre.gleichner@gmail.com>
2025-04-09 20:01:52 +0200

    Usage output

--- src/xattr-win.cpp
+++ src/xattr-win.cpp
@@ -98,6 +98,83 @@ public:
 
     void Usage()
     {
+        std::cout << R"(
+Similar to the commands of the Linux xattr, the following arguments may be supplied:
+
+List only the names of all EAs on the given file(s):
+```
+xattr [-lrvx] file [ file ... ]
+```
+Print only the value of EA ea_name on the given file(s):
+```
+xattr -p [-lrvx] ea_name file [ file ... ]
+```
+Write the value of the EA ea_name to ea_value:
+```
+xattr -w [-frux] ea_name ea_value file [ file ... ]
+    (No output on success, error messages on stderr.)
+```
+Delete the EA ea_name from file(s):
+```
+xattr -d [-r] ea_name file [ file ... ]
+    (No output on success, error messages on stderr.)
+```
+Clear all EA from the given file(s):
+```
+xattr -c [-r] file [ file ... ]
+    (No output on success, error messages on stderr.)
+```
+Options:
+
+    -c  Clear all Attributes.
+
+    -d  Delete the given attribute.
+
+    -f  Write content of a given file as EA value. Max size is 65565 bytes.
+        Use with -w option. This option can not be combined with -x or -u.
+
+    -h  Help.
+
+    -k  Keep-going. Ignore errors and continue processing the next file. 
+        This is useful when you want to process a list of files and ignore errors on some of them.
+
+    -l  By default, the first two command forms either display just the attribute names or
+        values, respectively. The -l option causes both the attribute names and corresponding
+        values to be displayed. For hex display of values, the output is preceeded with the hex
+        offset values and followed by ASCII display, enclosed by '|'.
+
+    -p  Print the value associated with the given attribute.
+
+    -r  If a file argument is a directory, act as if the entire contents of the directory
+        recursively were also specified (so that every file in the directory tree is acted upon).
+
+    -u  Write value as unicode string (UTF-16LE). The Default is UTF-8. 
+        This option can not be combined with -x.
+
+    -v  View the file name, even for a single file.
+
+    -w  Write a given attribute name with a value.
+
+    -x  Force the attribute value to be displayed in the hexadecimal representation.
+        This option can not be combined with -u.
+
+    --debug  Print debug information to stdout.
+
+One or more files or directories may be specified on the command line.
+
+For the first two forms of the command, if there is more than one file, 
+the file name is displayed along with the actual results. 
+When only one file is specified, the display of the file name is suppressed unless the -v option, is also specified.
+
+Attribute values are usually displayed as strings. However, if unprintable data are detected, the value is displayed in a hexadecimal representation.
+
+The -w option normally assumes the input attribute value is a string. 
+Specifying the -x option causes xattr to expect the input in hexadecimal (whitespace is ignored). 
+The hex bytes must be enclosed in "".
+
+xattr exits with 0 on success.
+On error it exits with a non-zero value and prints an error message on stderr.
+)";
     }
 
 private:
